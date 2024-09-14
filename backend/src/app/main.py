5aa4a5c80db94ce0cbbe2380eb785c1dd99a8436
Ruan Santiago <ruanvansantiago@gmail.com>
2024-09-14 13:21:57 -0300

    Adding legislator route

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,9 +1,10 @@
 
 from fastapi import FastAPI
+from routes import legislator_route as legislator
 import uvicorn
 
 
 app = FastAPI()
-
+app.include_router(legislator.router)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
