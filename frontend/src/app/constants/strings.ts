54845d00ad28733f88a5d4f8c044475a63463fe7
Ruan Santiago <ruanvansantiago@gmail.com>
2024-09-14 16:59:28 -0300

    Adding Constant string

---
+++ frontend/src/app/constants/strings.ts
@@ -0,0 +1,15 @@
+export const STRINGS = {
+    PAGE_TITLE: "Quorum Voting Analysis",
+    TABS: {
+      LEGISLATOR_VOTES: "Legislator Votes",
+      BILL_VOTES: "Bill Votes"
+    },
+    LEGISLATOR_SECTION: {
+      TITLE: "Legislator Voting Patterns",
+      SUBTITLE: "Support and opposition for bills by each legislator"
+    },
+    BILL_SECTION: {
+      TITLE: "Bill Voting Results",
+      SUBTITLE: "Support and opposition for each bill"
+    }
+  };
